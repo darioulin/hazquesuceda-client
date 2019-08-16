cb8363d59f10446fb05b4ba210b37d665d1d7926
darioulin <dariomuse95@gmail.com>
2019-08-16 11:56:28 -0500

    keep it magic

---
+++ src/views/LandingPage/LandingPage.jsx
@@ -0,0 +1,103 @@
+/*!
+
+=========================================================
+* Material Kit React - v1.7.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-kit-react
+* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/material-kit-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+// nodejs library to set properties for components
+import PropTypes from "prop-types";
+// react components for routing our app without refresh
+import { Link } from "react-router-dom";
+// nodejs library that concatenates classes
+import classNames from "classnames";
+// @material-ui/core components
+import withStyles from "@material-ui/core/styles/withStyles";
+
+// @material-ui/icons
+
+// core components
+import Header from "components/Header/Header.jsx";
+import Footer from "components/Footer/Footer.jsx";
+import GridContainer from "components/Grid/GridContainer.jsx";
+import GridItem from "components/Grid/GridItem.jsx";
+import Button from "components/CustomButtons/Button.jsx";
+import HeaderLinks from "components/Header/HeaderLinks.jsx";
+import Parallax from "components/Parallax/Parallax.jsx";
+
+import landingPageStyle from "assets/jss/material-kit-react/views/landingPage.jsx";
+
+// Sections for this page
+import ProductSection from "./Sections/ProductSection.jsx";
+import TeamSection from "./Sections/TeamSection.jsx";
+import WorkSection from "./Sections/WorkSection.jsx";
+import "./lp.css";
+const dashboardRoutes = [];
+
+class LandingPage extends React.Component {
+  render() {
+    const { classes, ...rest } = this.props;
+    return (
+      <div>
+        <Header
+          color="transparent"
+          routes={dashboardRoutes}
+          brand="Haz Que Suceda"
+          rightLinks={<HeaderLinks />}
+          fixed
+          changeColorOnScroll={{
+            height: 400,
+            color: "white"
+          }}
+          {...rest}
+        />
+        <Parallax filter image={require("assets/img/1.jpg")}>
+          <div className={classes.container}>
+            <GridContainer>
+              <GridItem xs={12} sm={12} md={6}>
+                <h1 className={classes.title}>Tu historia transciende aquí</h1>
+                <h4>
+                  Genera ingresos recurrentes y predecibles en tu ONG
+                  integrando un modelo de membresias 100% automatizado.
+                </h4>
+                <br />
+                <Link to={"/login"} className={classes.link}>
+                  <Button color="success" size="lg">
+                    <i className="fas fa-sign-in-alt" />
+                    Acceder
+                  </Button>
+                </Link>
+
+              </GridItem>
+            </GridContainer>
+          </div>
+        </Parallax>
+        <div className={classNames(classes.main, classes.mainRaised)}>
+          <div className={classes.container}>
+            <ProductSection />
+            <TeamSection />
+            <WorkSection />
+          </div>
+        </div>
+        <Footer />
+      </div>
+    );
+  }
+}
+
+LandingPage.propTypes = {
+  classes: PropTypes.object
+};
+
+export default withStyles(landingPageStyle)(LandingPage);
